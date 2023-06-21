bc59324a39102e70eeac99c2ba2b2f378e382705
hsomvanshi <hsomvanshi@paramvidya.iitpkd.ac.in>
2023-06-21 17:28:17 +0530

    Added Step2

---
+++ Day3/MD/Step2/Step2-Code1-Init-Vel.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_randist.h>
+     
+int main (void)
+{
+	int i, n;
+	double T, sigma;
+      	const gsl_rng_type * Q;
+
+	//Random Number Related
+	gsl_rng * r;
+	Q = gsl_rng_mt19937; //MT RAND. NUM. GENERATOR //Q = gsl_rng_default;
+       	r = gsl_rng_alloc (Q);
+      	gsl_rng_set(r, 100.0);
+	gsl_rng_env_setup();
+
+	//Initialize constant
+	n = 64;
+
+	//Initialize velocities
+	for(i=1; i<=n; i++){
+           double u = gsl_ran_gaussian(r, sigma) + 0.0; //GAUSSIAN WITH SD sigma AND MEAN 0.0
+     	   printf("%f\n", u);
+    	}
+  
+	gsl_rng_free (r);
+  
+	return 0;
+}
