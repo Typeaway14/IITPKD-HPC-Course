e59bda03f96f24b9ab386060600655c43d1db1cc
Typeaway14 <harsh.som33@gmail.com>
2023-06-21 19:20:37 +0530

    Correction to the Step2 codes

--- Day3/MD/Step2/Step2-Code1-Init-Vel.c
+++ Day3/MD/Step2/Step2-Code1-Init-Vel.c
@@ -1,32 +1,33 @@
+#include <gsl/gsl_randist.h>
+#include <gsl/gsl_rng.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#include <gsl/gsl_rng.h>
-#include <gsl/gsl_randist.h>
-     
-int main (void)
-{
-	int i, n;
-	double T, sigma;
-      	const gsl_rng_type * Q;
 
-	//Random Number Related
-	gsl_rng * r;
-	Q = gsl_rng_mt19937; //MT RAND. NUM. GENERATOR //Q = gsl_rng_default;
-       	r = gsl_rng_alloc (Q);
-      	gsl_rng_set(r, 100.0);
-	gsl_rng_env_setup();
+int main(void) {
+  int i, n;
+  double T, sigma = sqrt(1.5);
 
-	//Initialize constant
-	n = 64;
+  const gsl_rng_type *Q;
 
-	//Initialize velocities
-	for(i=1; i<=n; i++){
-           double u = gsl_ran_gaussian(r, sigma) + 0.0; //GAUSSIAN WITH SD sigma AND MEAN 0.0
-     	   printf("%f\n", u);
-    	}
-  
-	gsl_rng_free (r);
-  
-	return 0;
+  // Random Number Related
+  gsl_rng *r;
+  Q = gsl_rng_mt19937; // MT RAND. NUM. GENERATOR //Q = gsl_rng_default;
+  r = gsl_rng_alloc(Q);
+  gsl_rng_set(r, 100.0);
+  gsl_rng_env_setup();
+
+  // Initialize constant
+  n = 64;
+
+  // Initialize velocities
+  for (i = 1; i <= n; i++) {
+    double u =
+        gsl_ran_gaussian(r, sigma) + 0.0; // GAUSSIAN WITH SD sigma AND MEAN 0.0
+    printf("%f\n", u);
+  }
+
+  gsl_rng_free(r);
+
+  return 0;
 }
